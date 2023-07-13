5e74cacf4f64ee7ac056839def7a4b12b437b85c
Douglas Amaral <douglaxz@hotmail.com>
2023-07-13 09:25:50 -0300

    criação da funcionalidade add park

--- src/modules/price/store/index.js
+++ src/modules/price/store/index.js
@@ -4,20 +4,20 @@ import { create, list, detail, update } from "../api";
 export const priceStore = reactive({
   items: [],
   selectedModel: {},
-  async getModels() {
+  async getPrices() {
     const res = await list();
     this.items = res;
   },
-  async getModel(id) {
+  async getPrice(id) {
     const res = await detail(id);
     this.selectedPrice = res;
   },
-  async addModel(payload) {
+  async addPrice(payload) {
     const res = await create(payload);
     this.items.push(res);
     return res;
   },
-  async updateModel(id, payload) {
+  async updatePrice(id, payload) {
     const res = await update(id, payload);
     this.items = res;
   },
